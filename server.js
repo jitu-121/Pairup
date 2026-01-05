a1c8a71313d87d6faaf8d410e4a0ffbc8fe7933d
jitu-121 <baravkarjitendra66@gmail.com>
2026-01-06 00:56:52 +0530

    updated the authentication

--- server.js
+++ server.js
@@ -2,34 +2,12 @@ const express = require("express");
 
 const app = express();
 
-// Middleware to parse JSON bodies
-app.use(express.json());
-
-// GET /user
-app.get("/user", (req, res) => {
-  res.send({
-    firstName: "Akshay",
-    lastName: "Saini"
-  });
+// Middleware to parse JSON bodiess
+app.use("/user",(req ,res)=>{
+    res.send("Route Handler 1"); 
 });
 
-// POST /user
-app.post("/user", async (req, res) => {
-  console.log(req.body);
 
-  // saving data to DB (mock)
-  res.send("Data successfully saved to the database!");
-});
-
-// DELETE /user
-app.delete("/user", (req, res) => {
-  res.send("Deleted successfully!");
-});
-
-// This will match ALL HTTP methods for /test
-app.use("/test", (req, res) => {
-  res.send("Hello from the server!");
-});
 
 // Start server
 app.listen(3000, () => {
